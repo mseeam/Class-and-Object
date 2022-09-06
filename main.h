adfe4722694b01c1b55d228dfaa787ac04468e44
mseeam <mseeam99@gmail.com>
2022-09-06 07:05:16 -0500

    main.h includes includes sphere.h and functions.h

---
+++ main.h
@@ -0,0 +1,17 @@
+/********************************************************************************
+ * File  :   main.h                                                             *
+ * Author: Mohammad Seeam                                                       *
+ * Assignment 3: Classes and Objects                                            *
+ * This is a simple application to demonstrate classes and objects in C++.      *
+ * This header structure and files for all cpp and h files.                     *
+ * This file, main.h, includes sphere.h, functions.h                            *
+ * This file, main.h, contains all directives for main.cpp.                     *
+ *******************************************************************************/
+
+#ifndef MAIN_CLASSES_H
+#define MAIN_CLASSES_H
+
+#include "sphere.h"
+#include "functions.h"
+
+#endif /* MAIN_CLASSES_H */
