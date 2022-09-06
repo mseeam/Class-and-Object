95c106ec8611594794d0a113a71a4e3d3042fdfe
mseeam <mseeam99@gmail.com>
2022-09-06 07:07:30 -0500

    Calls display functions

---
+++ functions.h
@@ -0,0 +1,19 @@
+/********************************************************************************
+ * File  :   functions.h                                                        *
+ * Author: Mohammad Seeam                                                       *
+ * Assignment 3: Classes and Objects                                            *
+ * This is a simple application to demonstrate classes and objects in C++.      *
+ * This header structure and files for all cpp and h files.                     *
+ * This file, functions.h  , includes sphere.h                                  *
+ *******************************************************************************/
+
+#ifndef SPHERE_FUNCTIONS_H
+#define SPHERE_FUNCTIONS_H
+
+#include <iostream>
+#include "sphere.h"
+
+void displayCircle(Circle*);
+void displaySphere(Sphere*);
+
+#endif //SPHERE_FUNCTIONS_H
